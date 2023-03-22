022c0b62554cbdf0d332a9e7e32aca032694d85a
Tomas <viltetomas2003@gmail.com>
2023-03-22 17:50:22 -0300

    Obtener usuario por id

--- 04-go-mysql/main.go
+++ 04-go-mysql/main.go
@@ -14,8 +14,13 @@ func main() {
 	//user := models.CreateUser("Alexis", "Alexis123", "Alexis@mail.com")
 	//fmt.Println(user)
 
-	users := models.ListUsers()
-	fmt.Println(users)
+	// Obtener varios usuarios
+	//users := models.ListUsers()
+	//fmt.Println(users)
+
+	// Obtener usuario proporcionado el id
+	user := models.GetUser(3)
+	fmt.Println(user)
 
 	//db.TruncateTable("users")
 	db.Close()
--- 04-go-mysql/models/users.go
+++ 04-go-mysql/models/users.go
@@ -53,3 +53,14 @@ func ListUsers() Users {
 	}
 	return users
 }
+
+// Obtener un registro
+func GetUser(id int) *User {
+	user := NewUser("", "", "")
+	sql := "SELECT id, username, password, email FROM users WHERE id=?"
+	rows, _ := db.Query(sql, id)
+	for rows.Next() {
+		rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+	}
+	return user
+}
